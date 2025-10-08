e996a0caef3cf7ed3685ea559936fe0b48d454f6
jendralhxr <jendral.hxr@gmail.com>
2025-10-08 17:27:29 +0700

    soal UTS analisis numerik

---
+++ topografi.py
@@ -0,0 +1,66 @@
+# Tugas pengganti UTS
+# Analisis Numerik (A)
+# untuk dikumpulkan 14 Oktober 2025
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+NPM=5599
+np.random.seed(NPM)
+
+# grid
+nx, ny = 400, 400
+x = np.linspace(-10, 10, nx)
+y = np.linspace(-10, 10, ny)
+X, Y = np.meshgrid(x, y)
+
+# puncak/lembah berdasarkan fungsi eksponen
+# x0, y0: posisi
+# sx, sy: sebaran lelehan tumpengnya
+# amp   : amplitudo
+def peak(x0, y0, sx, sy, amp):
+    return amp * np.exp(-(((X - x0)**2) / (2*sx**2) + ((Y - y0)**2) / (2*sy**2)))
+
+Z = peak(-3, -2, 1.2, 1.8, 3.2) + peak(2.5, 2.5, 1.0, 1.0, 4) + peak(0, 0, 3.5, 3.5, 1.2) \
+    + peak(6, 6, 4, 4, -0.6)  + peak(-10, 8, 3.5, 3.5, -1.2) +  peak(-6, -6, 3, 3, -1.0) \
+    + peak(-2, -2, 4, 2, -0.6)  + peak(2.5, 9.5, 6, 6, 0.75)
+
+# exponential decay seperti global slope 
+Z += 0.5 * np.exp(-(np.sqrt((X/20)**2 + (Y/10)**2)))
+
+# variasi ripple kontur (acak)
+Z += 0.05 * np.random.randn(*Z.shape)
+
+# plot
+fig, ax = plt.subplots(figsize=(8, 7))
+levels = 18  # number of contour levels
+cf = ax.contourf(X, Y, Z, levels=levels)
+cs = ax.contour(X, Y, Z, levels=levels, linewidths=0.6)
+ax.clabel(cs, fmt="%.1f", fontsize=8)
+ax.set_title("Peta topografi")
+ax.set_xlabel("X (km)")
+ax.set_ylabel("Y (km)")
+ax.set_aspect('equal', adjustable='box')
+plt.colorbar(cf, ax=ax, label="elevasi (m)")
+
+# bayangkan peta ini adalah topografi bukit di suatu daerah
+# 
+# 1) cari titik tertinggi global  (bobot 10%)
+#   gunakan metode optimasi titik maksimum
+# 
+# 2) cari titik aliran 'sungai' menuju titik terendah, asumsikan jika 'hujan' langsung mengalir dan tidak terserap ke tanah (bobot 25%)
+#   gunakan metode optimasi titik minimum yang 'bertahap' mengalir atau melompat
+#   jalankan beberapa kali untuk melihat agregasi aliran pencarian tsb, visualisasikan
+#
+# 3) kerawanan erosi ditentukan oleh kecuraman lereng, hitung skor kerawanan di tiap titik (bobot 25%) 
+#   hitung dengan turunan pertama kontur topologi, boleh gunakan np.gradient() 
+#   nilai plus yang menggunakan fungsi numerik racikan sendiri untuk turunan parsialnya, yang kemudian dihitung resultannya
+# 
+# 4a) jika tanah hendak diratakan, berapa banyak volume tanah yang harus dipindahkan? (dipotong dari puncak dan diurug ke lembah) (bobot 20+5%)
+#   hitung dengan integral untuk sisi atas dan sisi bawah
+# 4b) berapa ketinggian akhir tanah yang rata tersebut?
+#
+# bonus (15%)
+#   pembahasan/diskusi yang kritis atau visualisasi yang ciamik (fancy, padat informasi, tapi tidak menyesatkan)
+#   pengerjaan yang gercep (2 poin per hari lebih cepat dari 14 Oktober, penalti 2% per hari sejak 15 Oktober jika terlambat)
+
